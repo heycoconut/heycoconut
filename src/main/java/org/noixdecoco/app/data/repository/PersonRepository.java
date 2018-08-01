73df24d993765bee39cc1c199e5c12f0fc4886cd
alecc08 <14881741+alecc08@users.noreply.github.com>
2018-08-01 16:05:55 -0400

    Fix import

--- src/main/java/org/noixdecoco/app/data/repository/PersonRepository.java
+++ src/main/java/org/noixdecoco/app/data/repository/PersonRepository.java
@@ -3,7 +3,7 @@ package org.noixdecoco.app.data.repository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.alec.app.data.model.Person;
+import org.noixdecoco.app.data.model.Person;
 
 @Repository
 public interface PersonRepository extends ReactiveMongoRepository<Person, Long> {
