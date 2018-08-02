98a9205ce76cca7812c7184f331852f80a5a9ead
alecc08 <14881741+alecc08@users.noreply.github.com>
2018-08-01 22:22:59 -0400

    Main class run sprint boot app

--- src/main/java/org/noixdecoco/app/App.java
+++ src/main/java/org/noixdecoco/app/App.java
@@ -1,13 +1,14 @@
 package org.noixdecoco.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@SpringBootApplication
+@EnableReactiveMongoRepositories
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
 }
