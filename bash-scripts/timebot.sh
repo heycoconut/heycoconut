26d1ad8789de4561f92865bced5ac035b01f22e2
Chou Trang <trang.chou@gmail.com>
2018-08-05 14:24:20 -0400

    update .gitignore and add timebot.sh to keep the service alive

---
+++ bash-scripts/timebot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ENDPOINT_URL='https://heycoconut.herokuapp.com/health'
+ACTIVE_TIME_START="6:00"
+ACTIVE_TIME_STOP="23:00"
+PING_INTERVAL=1200
+
+#24h - stop time - start time = hours to sleep * 3600 = seconds to sleep
+SLEEP_TIME="$((18 * 3600))"
+
+echo "###############################################"
+echo "STARTING THE TIMEBOT!"
+echo "PING_INTERVAL: ${PING_INTERVAL}"
+echo "Active start time: $ACTIVE_TIME_START"
+echo "Active stop time: $ACTIVE_TIME_STOP"
+echo "Will sleep for $SLEEP_TIME seconds, when not in the active hours"
+
+currenttime=$(date +%H:%M)
+echo "It's currently $currenttime"
+echo "###############################################"
+
+{
+  while :; do
+   currenttime=$(date +%H:%M)
+   if [[ "$currenttime" > "$ACTIVE_TIME_START" ]] || [[ "$currenttime" < "$ACTIVE_TIME_STOP" ]]; then
+
+     echo "[$(date)]"; curl $ENDPOINT_URL; echo ""
+     sleep $PING_INTERVAL
+   else
+     echo "It's sleeping time! I will sleep for ${SLEEP_TIME} secondes"
+     sleep $SLEEP_TIME
+   fi
+  done
+}
