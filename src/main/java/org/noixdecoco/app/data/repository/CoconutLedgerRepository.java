3778adf7209f0088250cf7a21e20a24f30d675a1
alecc08 <14881741+alecc08@users.noreply.github.com>
2018-08-21 20:05:03 -0400

    Fix deploy...

--- src/main/java/org/noixdecoco/app/data/repository/CoconutLedgerRepository.java
+++ src/main/java/org/noixdecoco/app/data/repository/CoconutLedgerRepository.java
@@ -1,7 +1,6 @@
 package org.noixdecoco.app.data.repository;
 
 import org.noixdecoco.app.data.model.CoconutLedger;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +10,4 @@ import reactor.core.publisher.Flux;
 public interface CoconutLedgerRepository extends ReactiveMongoRepository<CoconutLedger, Long> {
 	Flux<CoconutLedger> findByUsername(String username);
 
-	@Query()
-	Flux<CoconutLedger> getUsersByTotalCoconutCount(int limit);
 }
