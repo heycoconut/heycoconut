d81f14867b36d61ea0c2979d64c8975728e3bcbe
alecc08 <14881741+alecc08@users.noreply.github.com>
2018-10-10 18:43:30 -0400

    Add banned users option. Ignore slackbot messages

--- src/main/java/org/noixdecoco/app/rest/MainREST.java
+++ src/main/java/org/noixdecoco/app/rest/MainREST.java
@@ -9,6 +9,7 @@ import org.noixdecoco.app.command.manager.CoconutCommandManager;
 import org.noixdecoco.app.dto.SlackRequestDTO;
 import org.noixdecoco.app.utils.SlackSignatureUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -30,6 +31,9 @@ public class MainREST {
 
     private static final int EVENT_ID_QUEUE_SIZE = 200;
 
+    @Value("${banned.users}")
+    private String[] bannedUsers;
+
     public MainREST() {
         // Used to prevent events from being processed multiple times
         treatedEventIds = EvictingQueue.create(EVENT_ID_QUEUE_SIZE);
@@ -44,7 +48,7 @@ public class MainREST {
         }
         if (request.getChallenge() != null) {
             LOGGER.info("Getting challenged:" + request.getChallenge());
-        } else if (request.getEvent() != null && !treatedEventIds.contains(request.getEventId()) && signatureUtil.signatureIsValid(headers, bodyString)) {
+        } else if (validateRequest(request, headers, bodyString)) {
             // Add eventId to treatedEventIds to prevent reprocessing
             treatedEventIds.add(request.getEventId());
             LOGGER.info(request.toString());
@@ -55,6 +59,22 @@ public class MainREST {
         }
 
         return Flux.just(request);
+    }
+
+    private boolean validateRequest(SlackRequestDTO request, HttpHeaders headers, String bodyString) {
+        if(!treatedEventIds.contains(request.getEventId()) && signatureUtil.signatureIsValid(headers, bodyString)) {
+            if(bannedUsers != null || bannedUsers.length == 0) {
+                for (String bannedUser : bannedUsers) {
+                    if(request.getEvent().getUser().equals(bannedUser)) {
+                        LOGGER.info("Ignoring banned user: " + bannedUser);
+                        return false;
+                    }
+                }
+            }
+            // TODO: Check if bot user
+            return true;
+        }
+        return false;
     }
 
     private SlackRequestDTO extractRequestFromBody(String body) {
--- src/main/java/org/noixdecoco/app/service/UserService.java
+++ src/main/java/org/noixdecoco/app/service/UserService.java
@@ -3,4 +3,6 @@ package org.noixdecoco.app.service;
 public interface UserService {
 
     boolean isAdmin(String userId);
+
+    boolean isBot(String userId);
 }
