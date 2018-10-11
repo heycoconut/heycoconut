13ed253d994fb1eb66b1e6d56593d4123afee300
alecc08 <14881741+alecc08@users.noreply.github.com>
2018-10-11 18:09:45 -0400

    Add 2 new commands

---
+++ src/main/java/org/noixdecoco/app/command/ExistentialCommand.java
@@ -0,0 +1,46 @@
+package org.noixdecoco.app.command;
+
+import org.noixdecoco.app.command.annotation.Command;
+import org.noixdecoco.app.data.facts.CoconutFacts;
+import org.noixdecoco.app.dto.EventType;
+import org.noixdecoco.app.dto.SlackRequestDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Random;
+import java.util.function.Predicate;
+
+@Command(EventType.APP_MENTION)
+public class ExistentialCommand extends CoconutCommand {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExistentialCommand.class);
+
+    private String channel;
+
+    private Random random;
+
+    private ExistentialCommand(String user, String channel) {
+        super(user);
+        this.channel = channel;
+        random = new Random(System.currentTimeMillis());
+    }
+
+    public static Predicate<SlackRequestDTO> getPredicate() {
+        return r -> r.getEvent().getText().contains("who are you");
+    }
+
+    public static CoconutCommand build(SlackRequestDTO request) {
+        return new ExistentialCommand(request.getEvent().getUser(), request.getEvent().getChannel());
+    }
+
+    @Override
+    protected boolean validate() {
+        return channel != null;
+    }
+
+    @Override
+    protected void performAction() {
+        slackService.sendMessage(channel, "I'm the one and only HeyCoconut! I bring joy to the world in the form of virtual coconuts.");
+    }
+}
